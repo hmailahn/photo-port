e9e0ebcede2485d1f280618665314a7b2dffbb5e
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-04-07 18:54:50 -0500

    console log works for filling in form, need to work on submit now

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,27 +1,40 @@
+import React, { useState } from 'react';
+// import { validateEmail } from '../../utils/helpers';
+
+
 function ContactForm() {
 
-    return (
-        <section>
-          <h1>Contact me</h1>
-          <form id="contact-form">
-          <div>
-  <label htmlFor="name">Name:</label>
-  <input type="text" name="name" />
-</div>
-<div>
-  <label htmlFor="email">Email address:</label>
-  <input type="email" name="email" />
-</div>
-<div>
-  <label htmlFor="message">Message:</label>
-  <textarea name="message" rows="5"  />
-  <button type="submit">Submit</button>
-</div>
-          </form>
-        </section>
-      )
-    }
-    
-    export default ContactForm;
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
-    
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+      }
+      
+      console.log(formState);
+      
+
+  return (
+    <section>
+      <h1>Contact me</h1>
+      <form id="contact-form">
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" defaultValue={name} onChange={handleChange} name="name" />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input type="email" defaultValue={email} onChange={handleChange} name="email" />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" defaultValue={message} onChange={handleChange} rows="5"  />
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </section>
+  );
+}
+
+export default ContactForm;
